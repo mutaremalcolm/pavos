92c752e4f314717bd5818f03c1760c86b6784a4e
“malcolmmutare” <mutarem@gmail.com>
2025-11-01 21:55:46 +0200

    feat: implement routing structure with landing page and dashboard
    - Configured routes and routing groups for the PAVOS app
    - Added landing page and dashboard pages
    - Organized routes to support authentication and improve code structure

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <p>Dashboard Page (Protected)</p>
+  );
+}
+
+export default DashboardPage
+ 
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Landingpage = () => {
+  return (
+    <div>
+        Landingpage (Unprotected)
+    </div>
+  )
+}
+
+export default Landingpage
